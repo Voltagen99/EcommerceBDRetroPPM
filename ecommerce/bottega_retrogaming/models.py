d7c63056b89fc3e87abc428665140ec05a574b44
Francesco Moschettini <89149388+Voltagen99@users.noreply.github.com>
2025-06-22 16:21:15 +0200

    Testing orders (fixed bugged orders)

--- ecommerce/bottega_retrogaming/models.py
+++ ecommerce/bottega_retrogaming/models.py
@@ -1,6 +1,6 @@
-from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import AbstractUser
+
 
 class CustomUser(AbstractUser):
     ADMIN = 'ADMIN'
@@ -16,41 +16,21 @@ class CustomUser(AbstractUser):
     )
     name = models.CharField("Nome visualizzato", max_length=200, blank=True, null=True)
 
-    groups = models.ManyToManyField(
-        Group,
-        verbose_name='groups',
-        blank=True,
-        help_text=(
-            'The groups this user belongs to. A user will get all permissions '
-            'granted to each of their groups.'
-        ),
-        related_name="customuser_groups",
-        related_query_name="user",
-    )
-
-    user_permissions = models.ManyToManyField(
-        Permission,
-        verbose_name='user permissions',
-        blank=True,
-        help_text='Specific permissions for this user.',
-        related_name="customuser_permissions",
-        related_query_name="user",
-    )
-
     def save(self, *args, **kwargs):
-        if self.user_type == self.ADMIN:
+        if self.is_superuser:
             self.is_staff = True
-            self.is_superuser = True
+            self.user_type = self.ADMIN
         else:
-            self.is_staff = False
-            self.is_superuser = False
+            if self.user_type == self.ADMIN:
+                self.is_staff = True
+            else:
+                self.is_staff = False
         super().save(*args, **kwargs)
 
     def __str__(self):
         if self.name:
             return self.name
         return self.username
-
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
@@ -71,7 +51,7 @@ class Product(models.Model):
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+    customer = models.ForeignKey('CustomUser', on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=100, null=True)
@@ -88,7 +68,7 @@ class OrderItem(models.Model):
 
 
 class ShippingAddress(models.Model):
-    customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    customer = models.ForeignKey('CustomUser', on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     address = models.CharField(max_length=200, null=False)
     city = models.CharField(max_length=200, null=False)
--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     }
 }
 
-
+AUTH_USER_MODEL = 'bottega_retrogaming.CustomUser'
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
