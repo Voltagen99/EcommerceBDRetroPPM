1515f23ed3d64a7e9f07756c429b7e3d7b4e12b4
Francesco Moschettini <89149388+Voltagen99@users.noreply.github.com>
2025-06-22 15:45:34 +0200

    Testing orders (not working at the moment)

--- ecommerce/bottega_retrogaming/admin.py
+++ ecommerce/bottega_retrogaming/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from .models import *
+from .models import CustomUser, Product, Order, OrderItem, ShippingAddress
 
-admin.site.register(CustomUser)
 admin.site.register(Product)
-admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
+admin.site.register(CustomUser)
+
+class OrderAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "customer":
+            kwargs["queryset"] = CustomUser.objects.filter(user_type=CustomUser.CUSTOMER)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    list_display = ('id', 'customer', 'date_ordered', 'complete')
+    list_filter = ('complete', 'date_ordered')
+
+admin.site.register(Order, OrderAdmin)
