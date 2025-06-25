86493b05d3d1f2e3e60ce38a40b922acf2fd4e95
Francesco Moschettini <89149388+Voltagen99@users.noreply.github.com>
2025-06-25 15:57:05 +0200

    Moved login view to user_management templates

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bottega_retrogaming.apps.BottegaRetrogamingConfig',
-    'user_management',
+    'user_management.apps.UserManagementConfig',
 ]
 
 MIDDLEWARE = [
