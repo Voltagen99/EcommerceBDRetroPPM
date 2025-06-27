2400bfd8c5bc89213959d48f5a070a810f8e0aec
Francesco Moschettini <89149388+Voltagen99@users.noreply.github.com>
2025-06-27 22:43:31 +0200

    Added Description to Product

--- ecommerce/bottega_retrogaming/models.py
+++ ecommerce/bottega_retrogaming/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     digital = models.BooleanField(default=False, null=True, blank=True)
+    description = models.CharField("Descrizione", max_length=500, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
