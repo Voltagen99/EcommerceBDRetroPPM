00075e6085e01d62b3244c427b6cf27f6dde21a2
Francesco Moschettini <89149388+Voltagen99@users.noreply.github.com>
2025-06-30 14:59:46 +0200

    allowed_host added

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(xom_f+x+fgtv+k*4xw&x3sl$f)z=h&ow39lpbc%^ycum*xi&t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bdretromoschettinippm.onrender.com']
 
 # Application definition
 
